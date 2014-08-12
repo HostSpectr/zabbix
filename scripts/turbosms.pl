3f5e2368c66387c827ad23929d0774554ef713fa
Alex (aka Jumuro) <hostspectr@gmail.com>
2014-08-12 15:42:42 +0400

    Add UTF-8 support

--- scripts/turbosms.pl
+++ scripts/turbosms.pl
@@ -3,6 +3,7 @@
 use strict;
 use POSIX;
 use DBI;
+use encoding 'utf8';
 
 my $phone=@ARGV[0];
 my $subj=@ARGV[1];
@@ -13,7 +14,7 @@ my $user = "MyLogin";
 my $password = "MyPassword";
 my $database = "users";
 my $dsn = "DBI:$driver:host=94.249.146.189:3306;database=$database";
-my $dbh = DBI->connect($dsn, $user, $password);
+my $dbh = DBI->connect($dsn, $user, $password, {mysql_enable_utf8 => 1});
 
 my $sql = $dbh->prepare("INSERT INTO HostspectrLogin (number, sign, message) VALUES ('$phone', 'MySignature', '$msg')");
 
